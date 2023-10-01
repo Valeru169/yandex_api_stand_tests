8650cf7a59125579d0116ab1cfcc0d5d9cf96d02
Valeru169 <144037266+Valeru169@users.noreply.github.com>
2023-10-02 00:27:34 +0700

    Add files via upload

--- configuration.py
+++
@@ -1,6 +0,0 @@
-URL_SERVICE = "https://7f725e79-6366-403f-b59d-fa7dbdc3e75e.serverhub.praktikum-services.ru"
-DOC_PATH = "/docs/"
-LOG_MAIN_PATH = "/api/logs/main/"
-USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
-CREATE_USER_PATH = "/api/v1/users/"
-PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
